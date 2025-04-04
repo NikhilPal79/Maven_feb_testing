bb1d76aa4b5fa655e201359d20c24d3684efe8d2
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-04 13:26:44 -0400

    testing maven

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Employee {
+
+
+
+    private int id;
+    private String name;
+    private String email;
+
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee(101, "nikhil","nikhil7@gmail.com");
+        System.out.println(e1);
+    }
+}
